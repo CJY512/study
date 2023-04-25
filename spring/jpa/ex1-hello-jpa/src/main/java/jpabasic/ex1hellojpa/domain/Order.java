2cfb0a5e08e4392e39a0030ec4a1ea8256915608
cjy <c0512jy@naver.com>
2023-04-26 00:47:02 +0900

    지연 로딩, CASCADE 설정

--- spring/jpa/ex1-hello-jpa/src/main/java/jpabasic/ex1hellojpa/domain/Order.java
+++ spring/jpa/ex1-hello-jpa/src/main/java/jpabasic/ex1hellojpa/domain/Order.java
@@ -8,6 +8,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.CascadeType.*;
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -17,17 +20,17 @@ public class Order extends BaseEntity {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     //주문서를 중심으로 어떤 아이템이 필요한 지.. 이 설정은 의미가 있다.
-    @OneToMany(mappedBy = "order")
-    private List<Order> orderItems = new ArrayList<>();
+    @OneToMany(mappedBy = "order", cascade = ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate;
 
