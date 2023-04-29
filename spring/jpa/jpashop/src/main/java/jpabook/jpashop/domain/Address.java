26847919a489d2a179eabe51c6e15029328b7ce3
cjy <c0512jy@naver.com>
2023-04-29 17:24:59 +0900

    기본 생성자 롬복으로 대체

--- spring/jpa/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ spring/jpa/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,19 +1,19 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //JPA에 필요한 기본 생성자 조건
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
-
-    protected Address() {
-    }
 
     public Address(String city, String street, String zipcode) {
         this.city = city;
